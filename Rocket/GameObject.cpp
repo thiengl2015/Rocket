b8e840da89a8225747af024285db41d482561978
thiengl2015 <giathien899@gmail.com>
2025-03-02 16:37:59 +0700

    Add project files.

---
+++ Rocket/GameObject.cpp
@@ -0,0 +1,65 @@
+﻿#include <d3dx9.h>
+
+#include "debug.h"
+#include "Game.h"
+#include "GameObject.h"
+
+/*
+	Initialize game object 
+*/
+CGameObject::CGameObject(float x, float y, LPTEXTURE tex)
+{
+	this->x = x;
+	this->y = y;
+	this->texture = tex;
+}
+
+void CGameObject::Render()
+{
+	CGame::GetInstance()->Draw(x, y, texture);
+}
+
+CGameObject::~CGameObject()
+{
+	if (texture != NULL) delete texture;
+}
+
+#define MARIO_VX 0.1f
+#define MARIO_WIDTH 14
+
+void Crocket::Update(DWORD dt)
+{
+	if (GetAsyncKeyState('A') & 0x8000) // Nhấn A -> Di chuyển trái
+		x -= vx * dt;
+	if (GetAsyncKeyState('D') & 0x8000) // Nhấn D -> Di chuyển phải
+		x += vx * dt;
+	if (GetAsyncKeyState('W') & 0x8000) // Nhấn W -> Di chuyển lên
+		y -= vx * dt;       // Trục y ngược chiều (0 ở trên cùng)
+	if (GetAsyncKeyState('S') & 0x8000) // Nhấn S -> Di chuyển xuống
+		y += vx * dt;
+
+	int BackBufferWidth = CGame::GetInstance()->GetBackBufferWidth();
+	if (x <= 0 || x >= BackBufferWidth - MARIO_WIDTH) {
+		
+		vx = -vx;
+
+		if (x <= 0)
+		{
+			x = 0;
+		}
+		else if (x >= BackBufferWidth - MARIO_WIDTH)
+		{
+			x = (float)(BackBufferWidth - MARIO_WIDTH);
+		}
+	}
+}
+void CBullet::Update(DWORD dt)
+{	
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000) // Nhấn SPACE để bắn đạn
+	{
+		if (active) // Chỉ di chuyển khi active
+		{
+			y -= vy * dt; // Viên đạn bay lên trên
+		}
+	}
+}
---
+++ Rocket/GameObject.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <Windows.h>
+#include <d3dx10.h>
+
+#include "Texture.h"
+
+class CGameObject
+{
+protected: 
+	float x; 
+	float y;
+
+	// This should be a pointer to an object containing all graphic/sound/audio assets for rendering this object. 
+	// For now, just a pointer to a single texture
+	LPTEXTURE texture;			
+public: 
+	void SetPosition(float x, float y) { this->x = x; this->y = y; }
+	float GetX() { return x; }
+	float GetY() { return y; }
+
+	CGameObject(float x = 0.0f, float y=0.0f, LPTEXTURE texture = NULL);
+
+	virtual void Update(DWORD dt) = 0;
+	virtual void Render();
+
+	~CGameObject();
+};
+typedef CGameObject * LPGAMEOBJECT;
+
+class CBrick : public CGameObject
+{
+public:
+	CBrick(float x, float y, LPTEXTURE texture): CGameObject(x,y,texture) {}
+	void Update(DWORD dt) {}; 
+};
+
+class Crocket : public CGameObject
+{
+protected:
+	float vx;
+	float vy;
+public: 
+	Crocket(float x, float y, float vx, float vy, LPTEXTURE texture) :CGameObject(x, y, texture)
+	{
+		this->vx = vx;
+		this->vy = vy;
+	};
+	void Update(DWORD dt);
+};
+
+class Cenamy : public CGameObject
+{
+public:
+	Cenamy(float x, float y, LPTEXTURE texture): CGameObject(x,y,texture) {}
+	void Update(DWORD dt);
+};
+
+class CBullet : public Crocket
+{
+private:
+	bool active = 0;
+public:
+	float GetVy() { return vy; }
+	void SetVy(float vy) { this->vy = vy; }
+	CBullet(float x, float y, float vx, float vy, LPTEXTURE texture) : Crocket(x, y, 0, vy, texture)
+	{
+		this->active = true;
+	};
+	void Update(DWORD dt);
+};
